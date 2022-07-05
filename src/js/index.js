cf0568792966e25d00087751fdf360f9e597107a
Digidr0 <ildar.grankin2@gmail.com>
2022-07-05 18:30:49 +0300

    redirect new "display data" to new file, favicon, style adds

--- src/js/index.js
+++ src/js/index.js
@@ -3,31 +3,10 @@ import "..//css/header.css";
 import "..//css/background.css";
 import "..//css/load.css";
 import "..//css/main.css";
-
-import checkDirection from "./checkDirection";
-import changeBG from "./changeBG";
-import {format, fromUnixTime,  utcToZonedTime} from "date-fns";
+import displayData from "./displayData";
 
 const searchInput = document.querySelector(".search.input");
 const searchBtn = document.querySelector(".search.btn");
-const responseTime = document.querySelector(".response-time");
-
-const load = document.querySelector(".lds-ellipsis");
-const city = document.querySelector(".city");
-const temperature = document.querySelector(".temperature");
-const temperatureMin = document.querySelector(".temperature-min");
-const temperatureMax = document.querySelector(".temperature-max");
-const wetatherStatus = document.querySelector(".weather-status");
-const wetatherStatusIcon = document.querySelector(".weather-status-icon");
-
-const feels = document.querySelector(".feels > span");
-const wind = document.querySelector(".wind > span");
-const windGust = document.querySelector(".wind-gust > span");
-const pressure = document.querySelector(".pressure > span");
-const visibility = document.querySelector(".visibility > span");
-const humidity = document.querySelector(".humidity > span");
-const sunrise = document.querySelector(".sunrise .stats");
-const sunset = document.querySelector(".sunset .stats");
 
 const KEY = "5943486d8d5be825b7f2cf0cc6b9375d";
 let params = new URLSearchParams({
@@ -61,30 +40,6 @@ async function getWeather(req) {
     console.error("DATA HAS NOT RECIVED!\n", err);
     displayError();
   }
-}
-
-function displayData(data) {
-  console.log(data);
-
-  city.textContent = `${data.name}, ${data.sys.country.toLowerCase()}`;
-  temperature.textContent = `${data.main.temp.toFixed(1)}°`;
-  temperatureMin.textContent = `${data.main.temp_min.toFixed(1)}°`;
-  temperatureMax.textContent = `${data.main.temp_max.toFixed(1)}°`;
-  wetatherStatus.textContent = `${data.weather[0].description.capFstLtr()}`;
-  wetatherStatusIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  changeBG(data.weather[0].icon, data.timezone);
-
-  feels.textContent = `${data.main.feels_like.toFixed(1)}°`;
-  wind.textContent = `${data.wind.speed} м/с, ${checkDirection(data.wind.deg)}`;
-  windGust.textContent = `${data.wind.gust} м/с`;
-  pressure.textContent = ` ${data.main.pressure} гПа`;
-  visibility.textContent = `${(data.visibility / 1000).toFixed(1)} км`;
-  humidity.textContent = `${data.main.humidity}%`;
-
-  sunrise.textContent = format(fromUnixTime(data.sys.sunrise), "hh:mm aa");
-  sunset.textContent = format(fromUnixTime(data.sys.sunset), "hh:mm aa");
-
-  responseTime.innerText = format(new Date(), "HH:mm");
 }
 
 function displayError() {
